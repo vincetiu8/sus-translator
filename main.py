04d6c06ef160d3bcbb3aeb5c991bfbc03fc37197
Vincent Tiu <46623413+vincetiu8@users.noreply.github.com>
2021-10-12 20:46:19 +0800

    fix file

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
+import random
 
 import discord
 
-token = os.environ["TOKEN"]
 shift = 32
 words = ["amogus", "sussy", "baka", "impostor", "amogsus", "sugondese", "daddy", "submissive", "breedable"]
-translations = ["mog"]
+translations = []
 for word in words:
     for start in range(len(word)):
         for end in range(start + 3, len(word) + 1):
@@ -14,9 +14,14 @@ for word in words:
                 continue
             translations.append(part)
 
+random.shuffle(translations)
+translations.remove("amogus")
+translations.insert(0, "amogus")
+
 max_char = len(translations)
 
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
@@ -29,24 +34,32 @@ async def on_message(message):
         return
 
     response = "ඞඞඞ Your translation is: "
-    if message.content.startswith("ඞtoamog"):
-        for char in message:
+    if message.content.startswith("ඞtoamog "):
+        for char in message.content.split("ඞtoamog ")[1]:
             index = ord(char) - shift
             if char == ' ':
                 index = 0
             if index >= len(translations):
-                message += translations[63 - shift]
+                response += translations[63 - shift] + " "
                 continue
-            response += translations[index]
-    elif message.content.startswith("ඞfromamog"):
-        parts = message.split(" ")
+            response += translations[index] + " "
+    elif message.content.startswith("ඞfromamog "):
+        parts = message.content.split("ඞfromamog ")[1].split(" ")
         for message_part in parts:
             for i, match in enumerate(translations):
                 if message_part == match:
                     if i == 0:
                         response += " "
-                    response += chr(i + shift)
+                    else:
+                        response += chr(i + shift)
                     break
+            else:
                 response += "?"
+    else:
+        response = "Invalid command, you sussy baka ඞඞඞ"
+
     await message.channel.send(response)
-    client.run(token)
+
+
+print(os.getenv("TOKEN"))
+client.run(os.getenv("TOKEN"))
